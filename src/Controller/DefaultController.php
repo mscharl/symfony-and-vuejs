7b5f6c35899155c986093e8ae19a617998b5ed4e
Michael Scharl <michael.scharl@massiveart.com>
2019-07-13 20:41:12 +0200

    Initial Symfony install

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function index() {
+        return $this->render('index.html.twig');
+    }
+}
